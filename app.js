d41060c44c1c0ef2ba7f1c0469767ab4c70b26e4
MarceloEsc <marceloescobarjunior@gmail.com>
2020-07-03 00:56:34 -0300

    debounce adicionado

--- app.js
+++ app.js
@@ -1,36 +1,64 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-let quant = 10
-let index = 0
+const quant = 50
+let index = 0,
+	timerId
 
-const generatePokemonPromises = () => Array(quant).fill().map((_, index) =>
-	fetch(getPokemonUrl(index + 1)).then((res) => res.json()))
+const generatePokemonPromises = (quant) => Array(quant).fill().map(() => {
+	index++
+	return fetch(getPokemonUrl(index)).then((res) => res.json())
+})
 
 const generateHTML = pokemons => pokemons.reduce((accumulator, { id, name, types, sprites }) => {
 	const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
 	accumulator += `<li class="card ${elementTypes[0]}">
 		<img class="card-image" alt="${name}" src="${sprites.front_default}"/>
-		<h2 class="car-title">${id}, ${name}</h2>
+		<h2 class="car-title">${id} - ${name}</h2>
 		<p class="card-subtitle">${elementTypes.join(' | ')}</p>
 	</li>`
 
 	return accumulator;
 }, '')
 
+// fazer a geração da página de layout do pokemon escolhido
+const generatePokemonLayout = pokemon => { }
+
 const insertPokemonsIntoPage = pokemons => {
 	const ul = document.querySelector('[data-js="pokedex"]')
 	ul.innerHTML += pokemons
 }
 
+// fazer a inserção de dados na página de layout do pokemon escolhido
+const insertPokemonIntoInfoPage = pokemon => { }
+
 const fetchPokedex = () => {
-	const pokemonPromises = generatePokemonPromises()
+	const pokemonPromises = generatePokemonPromises(quant)
 
 	Promise.all(pokemonPromises)
 		.then(generateHTML)
 		.then(insertPokemonsIntoPage)
-
-	quant += quant
-	index += quant
 }
 fetchPokedex()
-fetchPokedex()
+
+const debounce = (func) => {
+	if (timerId) return
+
+	func()
+	timerId = setTimeout(() => {
+		timerId = undefined
+	}, 2000);
+}
+
+window.onscroll = () => {
+	// if (index >= 150) return console.log('asd');
+	window.removeEventListener('scroll', this)
+
+	const { clientHeight, scrollHeight, scrollTop } = document.documentElement
+	let progresso = Math.abs((scrollTop / (clientHeight - scrollHeight)) * 100)
+	console.log(progresso);
+
+
+	if (progresso > 99) {
+		debounce(fetchPokedex)
+	}
+}
